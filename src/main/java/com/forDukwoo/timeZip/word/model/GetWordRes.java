658ded1493119f945baaa69b95e7e70caba9b97c
Park-EunBi <parkeb555@gmail.com>
2022-08-25 17:54:09 +0900

    FEAT get /words API 단어장에 저장된 단어 리스트 반환

---
+++ src/main/java/com/forDukwoo/timeZip/word/model/GetWordRes.java
@@ -0,0 +1,14 @@
+package com.forDukwoo.timeZip.word.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetWordRes {
+    String word;
+    String meaning1;
+    String meaning2;
+}
